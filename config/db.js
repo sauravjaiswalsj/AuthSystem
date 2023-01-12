f2a17e772bb2072317496e8c7a80ec4c9df86d20
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2023-01-12 14:16:05 +0530

    update

--- config/db.js
+++ config/db.js
@@ -11,4 +11,15 @@ const createDb = new Sequelize("test-db",user,password,{
     host:"./config/db.sqlite"
 });
 
+const sequelize = createDb;
+
+//authenticate the db
+sequelize.authenticate()
+    .then(()=>{
+        console.log(`Authenticated to DB successfully.`);
+    })
+    .catch(err=>{
+        console.log(`Error occured during db connection: ${err}`);
+});
+
 module.exports = createDb;
